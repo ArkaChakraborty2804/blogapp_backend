0234fef6adb3baeea253b29f25e594773a587168
Arka Chakraborty <arkachakraborty@Arkas-Air.mshome.net>
2024-01-10 21:08:10 +0530

    backend added

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const mongoose=require('mongoose')
-const express = require('express')
+import { connect } from 'mongoose'
+import express, { json  } from 'express'
 const app=express()
-const dotenv=require('dotenv')
-const cors=require('cors')
-const multer=require('multer')
-const path=require("path")
-const cookieParser=require('cookie-parser')
-const authRoute=require('./routes/auth')
-const userRoute=require('./routes/users')
-const postRoute=require('./routes/posts')
-const commentRoute=require('./routes/comments')
+import { config } from 'dotenv'
+import cors from 'cors'
+import multer, { diskStorage } from 'multer'
+import { join } from "path"
+import cookieParser from 'cookie-parser'
+import authRoute from './routes/auth'
+import userRoute from './routes/users'
+import postRoute from './routes/posts'
+import commentRoute from './routes/comments'
 
 //database
 const connectDB=async()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URL)
+        await connect(process.env.MONGO_URL)
         console.log("database is connected successfully!")
 
     }
@@ -26,9 +26,9 @@ const connectDB=async()=>{
 
 
 //middlewares
-dotenv.config()
-app.use(express.json())
-app.use("/images",express.static(path.join(__dirname,"/images")))
+config()
+app.use(json())
+app.use("/images",(join(__dirname,"/images")))
 app.use(cors({origin:"http://localhost:5173",credentials:true}))
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
@@ -37,7 +37,7 @@ app.use("/api/posts",postRoute)
 app.use("/api/comments",commentRoute)
 
 //image upload
-const storage=multer.diskStorage({
+const storage=diskStorage({
     destination:(req,file,fn)=>{
         fn(null,"images")
     },
